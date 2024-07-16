d51a780f565fca236371a4a4e1225c224c56b588
RogerIsk <rogeriskrenov@gmail.com>
2024-07-16 02:52:36 +0200

    calculator in progress

--- 15_calculator.py
+++ 15_calculator.py
@@ -1,33 +1,55 @@
 from kivy.app import App                # libraries and dependancies
 from kivy.uix.widget import Widget      # at first they were like 10+ but the deeper i went with the tutorials the less they got
-from kivy.properties import ObjectProperty
 from kivy.lang import Builder
 from kivy.core.window import Window
 from kivy.uix.image import Image
 from kivy.uix.floatlayout import FloatLayout
 
-Builder.load_file('update_lebels.kv') #this loads all the styling settings from 'box_layout'
+Window.size = (500, 700) #this sets the size of the window
+Builder.load_file('calculator.kv') #this loads all the styling settings from 'box_layout'
 
 
 
-#class that handles the whole main window
 class MyLayout(Widget):
-    def press(self):
-        # get the name(str) from Input text
-        name = self.ids.name_input.text
-        print("Hello", name)
+    def clear(self):
+        self.ids.calc_input.text = '0'
 
-        # update the label text by id
-        self.ids.my_label.text = "Hello, " + name
+    def press_one(self):
+        self.ids.calc_input.text.text = '1'
 
-        # clear the input box
-        self.ids.name_input.text = ""
+    def press_two(self):
+        self.ids.calc_input.text = '2'
+
+    def press_three(self):
+        self.ids.calc_input.text = '3'
+
+    def press_four(self):
+        self.ids.calc_input.text = '4'
+
+    def press_five(self):
+        self.ids.calc_input.text = '5'
+
+    def press_six(self):
+        self.ids.calc_input.text = '6'
+
+    def press_seven(self):
+        self.ids.calc_input.text = '7'
+
+    def press_eight(self):
+        self.ids.calc_input.text = '8'
+
+    def press_nine(self):
+        self.ids.calc_input.text = '9'
+
+    def press_zero(self):
+        self.ids.calc_input.text = '0'
+    
 
 #the class that runs the window and shows the window title automatically, for some reason kivy ignores 'App'
-class AwesomeApp(App): 
+class Basic_CalculatorApp(App): 
     def build(self):
         Window.clearcolor = (0, 0, 0, 1)
         return MyLayout()
     
 if __name__ == '__main__':
-    AwesomeApp().run()
+    Basic_CalculatorApp().run()
